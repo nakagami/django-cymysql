0d48c5ef5dcfa1647b200a8d9cafab9d5f56d601
Hajime Nakagami <nakagami@gmail.com>
2013-10-28 16:37:33 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+Django database backend for cymysql
+"""
+from distutils.core import setup, Command
+
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'Topic :: Database',
+]
+
+setup(name='mysql_cymysql', 
+        version='0.1.0',
+        description = __doc__, 
+        url='https://github.com/nakagami/django-cymysql/',
+        classifiers=classifiers,
+        keywords=['MySQL', 'cymysql'],
+        license='BSD',
+        author='Hajime Nakagami',
+        author_email='nakagami@gmail.com',
+        packages = ['mysql_cymysql'],
+        long_description=__doc__,
+)
