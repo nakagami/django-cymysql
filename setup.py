ebe0bcee792d3020a7fbe7c9bcf6461bad3043b0
Hajime Nakagami <nakagami@gmail.com>
2014-02-18 23:26:03 +0900

    revision number up.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ classifiers = [
 ]
 
 setup(name='django-cymysql', 
-        version='0.2.0',
+        version='0.2.1',
         description = __doc__, 
         url='https://github.com/nakagami/django-cymysql/',
         classifiers=classifiers,
