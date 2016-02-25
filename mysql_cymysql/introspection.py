5ee42f3736bd5928144f5fa3f7ca7612b6e4b4a4
Hajime Nakagami <nakagami@gmail.com>
2016-02-26 03:13:19 +0900

    fix get_table_description() for django 1.9.2

--- mysql_cymysql/introspection.py
+++ mysql_cymysql/introspection.py
@@ -8,7 +8,8 @@ from django.db.backends.base.introspection import (
 from django.utils.datastructures import OrderedSet
 from django.utils.encoding import force_text
 
-FieldInfo = namedtuple('FieldInfo', FieldInfo._fields + ('extra',))
+FieldInfo = namedtuple('FieldInfo', FieldInfo._fields + ('extra', 'default'))
+InfoLine = namedtuple('InfoLine', 'col_name data_type max_len num_prec num_scale extra column_default')
 
 
 class DatabaseIntrospection(BaseDatabaseIntrospection):
@@ -58,9 +59,9 @@ class DatabaseIntrospection(BaseDatabaseIntrospection):
         #   not visible length (#5725)
         # - precision and scale (for decimal fields) (#5014)
         # - auto_increment is not available in cursor.description
-        InfoLine = namedtuple('InfoLine', 'col_name data_type max_len num_prec num_scale extra')
         cursor.execute("""
-            SELECT column_name, data_type, character_maximum_length, numeric_precision, numeric_scale, extra
+            SELECT column_name, data_type, character_maximum_length, numeric_precision,
+                   numeric_scale, extra, column_default
             FROM information_schema.columns
             WHERE table_name = %s AND table_schema = DATABASE()""", [table_name])
         field_info = {line[0]: InfoLine(*line) for line in cursor.fetchall()}
@@ -77,7 +78,8 @@ class DatabaseIntrospection(BaseDatabaseIntrospection):
                                to_int(field_info[col_name].num_prec) or line[4],
                                to_int(field_info[col_name].num_scale) or line[5])
                             + (line[6],)
-                            + (field_info[col_name].extra,)))
+                            + (field_info[col_name].extra,)
+                            + (field_info[col_name].column_default,)))
             )
         return fields
 
