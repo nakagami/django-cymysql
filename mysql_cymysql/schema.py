b1e0bae7231009a7a5506bde03c48dbaf69edc17
Hajime Nakagami <nakagami@gmail.com>
2016-08-06 15:06:12 +0900

    fix typo

--- mysql_cymysql/schema.py
+++ mysql_cymysql/schema.py
@@ -4,4 +4,4 @@ from django.db.backends.mysql import schema
 class DatabaseSchemaEditor(schema.DatabaseSchemaEditor):
 
     def quote_value(self, value):
-        return cymysql.converter.escape_item(value, 'utf-8')
+        return cymysql.converters.escape_item(value, 'utf-8')
