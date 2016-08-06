9e0ace9e3130fb6f0e689f0e2910df590a43e00e
Hajime Nakagami <nakagami@gmail.com>
2016-08-06 16:59:25 +0900

    import in local scope.

--- mysql_cymysql/schema.py
+++ mysql_cymysql/schema.py
@@ -1,7 +1,7 @@
-import cymysql
 from django.db.backends.mysql import schema
 
 class DatabaseSchemaEditor(schema.DatabaseSchemaEditor):
 
     def quote_value(self, value):
+        import cymysql
         return cymysql.converters.escape_item(value, 'utf-8')
