d45eab3186655f9e7df6c9f3ae1886d129a2a517
Hajime Nakagami <nakagami@gmail.com>
2016-08-06 14:52:58 +0900

    fix quote_value()

--- mysql_cymysql/schema.py
+++ mysql_cymysql/schema.py
@@ -1,6 +1,7 @@
+import cymysql
 from django.db.backends.mysql import schema
 
 class DatabaseSchemaEditor(schema.DatabaseSchemaEditor):
 
     def quote_value(self, value):
-        return self.connection.escape(value)
+        return cymysql.converter.escape_item(value, 'utf-8')
