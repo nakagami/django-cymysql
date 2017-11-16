db2548ca92ddc86fe6c5c550b410851d8dd04ec7
Hajime Nakagami <nakagami@gmail.com>
2017-11-16 19:50:31 +0900

    remover python2 classifier

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ classifiers = [
     'License :: OSI Approved :: BSD License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 3',
     'Topic :: Database',
     'Framework :: Django',
