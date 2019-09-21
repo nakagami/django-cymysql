f9933bdbe2987fc8f6fcae5d923898802ca8c0dc
Hajime Nakagami <nakagami@gmail.com>
2019-09-21 15:13:01 +0900

    fix schema quote_value

--- mysql_cymysql/schema.py
+++ mysql_cymysql/schema.py
@@ -4,4 +4,6 @@ from django.db.backends.mysql import schema
 class DatabaseSchemaEditor(schema.DatabaseSchemaEditor):
 
     def quote_value(self, value):
+        if isinstance(value, str):
+            value = value.replace('%', '%%')
         return cymysql.converters.escape_item(value, 'utf-8')
