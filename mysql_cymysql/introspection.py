8c7c94bc1d898865a45bd83dbc032a4c4ea71643
Hajime Nakagami <nakagami@gmail.com>
2019-09-21 12:05:08 +0900

    _parse_constraint_columns() from django.db.backends.mysql

--- mysql_cymysql/introspection.py
+++ mysql_cymysql/introspection.py
@@ -148,6 +148,19 @@ class DatabaseIntrospection(BaseDatabaseIntrospection):
             return self.connection.features._mysql_storage_engine
         return result[0]
 
+    def _parse_constraint_columns(self, check_clause, columns):
+        check_columns = OrderedSet()
+        statement = sqlparse.parse(check_clause)[0]
+        tokens = (token for token in statement.flatten() if not token.is_whitespace)
+        for token in tokens:
+            if (
+                token.ttype == sqlparse.tokens.Name and
+                self.connection.ops.quote_name(token.value) == token.value and
+                token.value[1:-1] in columns
+            ):
+                check_columns.add(token.value[1:-1])
+        return check_columns
+
     def get_constraints(self, cursor, table_name):
         """
         Retrieve any constraints or keys (unique, pk, fk, check, index) across
