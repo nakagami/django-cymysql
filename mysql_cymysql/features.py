ccc659c2a1400a72fc22d73e1a0b4e4239c0212e
Hajime Nakagami <nakagami@gmail.com>
2021-04-07 08:11:28 +0900

    revert

--- mysql_cymysql/features.py
+++ mysql_cymysql/features.py
@@ -3,3 +3,4 @@ from django.db.backends.mysql.features import DatabaseFeatures as BaseDatabaseFe
 
 class DatabaseFeatures(BaseDatabaseFeatures):
     empty_fetchmany_value = []
+    supports_paramstyle_pyformat = False
